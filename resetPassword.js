a3e71599fd7538a589d25889ca1e530cac16eb2b
mrickerd24 <melissa.rickerd@hotmail.com>
2026-04-04 09:23:35 -0400

    MP-85 Hotfix 2

--- resetPassword.js
+++ resetPassword.js
@@ -2,6 +2,26 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { translations, setLanguage } from "./translations.js";
 
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const firebaseConfig = {
+    apiKey: "AIzaSyA-17uYmpblsb3b-NlB5_RK7ci7ZvUkH4Q",
+    authDomain: "momentum-performance.firebaseapp.com",
+    projectId: "momentum-performance",
+    storageBucket: "momentum-performance.firebasestorage.app",
+    messagingSenderId: "571184327943",
+    appId: "1:571184327943:web:a5df6568228ca686faa9a2",
+    measurementId: "G-4996PSTP69"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+
+
+
+
+
 document.getElementById("reset").addEventListener("click", () => {
     const email = document.getElementById("emailAddress").value;
 
@@ -17,5 +37,8 @@ document.getElementById("reset").addEventListener("click", () => {
 
     .catch((error) => {
         alert(error.message);
+
+    
+    })
     });
 });
